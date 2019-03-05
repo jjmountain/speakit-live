e220839902045b87cd3914566f1cd7801e721cb9
James Devereux <devereuxjj@gmail.com>
2019-03-05 13:55:17 +0900

    created controllers and routes

---
+++ app/controllers/time_trials_controller.rb
@@ -0,0 +1,2 @@
+class TimeTrialsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :teachers
   devise_for :students
-  root to: 'pages#home'
+  root to: 'lessons#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :lessons, only: [ :index, :show, :new, :create, :update ]  do
+    resources :attendances, only: [ :create ]
+    resources :time_trials, only: [ :create, :show, :update ]
+  end
+  resources :time_trials, only: [ :index ]
+  resources :attendances, only: [ :destroy ]
 end
