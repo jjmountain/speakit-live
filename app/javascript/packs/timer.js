efa74a0f9ad062db3feb1080d63d950f7cc5260a
James Devereux <devereuxjj@gmail.com>
2019-03-13 12:46:13 +0900

    teacher can update all hw with one button now

--- app/javascript/packs/timer.js
+++ app/javascript/packs/timer.js
@@ -10,48 +10,52 @@ let defaultTime;
 
 const timerForm = document.querySelector('#time-trial-seconds')
 
-const secondUpdateForm = document.querySelector('#time-trial-update-seconds')
+if (timerForm) {
+  const secondUpdateForm = document.querySelector('#time-trial-update-seconds')
 
-const secondsGoal = parseInt(document.querySelector('#seconds-goal').innerHTML, 10)
+  const secondsGoal = parseInt(document.querySelector('#seconds-goal').innerHTML, 10)
 
-document.querySelector('.startButton').addEventListener('click', function (event) {
-    const defaultTimeValue = document.querySelector('#time-trial-current-time').value
-    if (defaultTimeValue === "") {
-      defaultTime = 0
-    } else {
-      defaultTime = parseInt(defaultTimeValue, 10)
-    }
-    console.log(defaultTime)
-    timer.start({precision: 'secondTenths', startValues: {seconds: defaultTime}, target: {seconds: secondsGoal }});
-    document.querySelector('#time-trial-start button').click();
-    event.currentTarget.style = 'display:none'
-    document.querySelector('.stopButton').style = 'display:block'
-});
+  document.querySelector('.startButton').addEventListener('click', function (event) {
+      const defaultTimeValue = document.querySelector('#time-trial-current-time').value
+      if (defaultTimeValue === "") {
+        defaultTime = 0
+      } else {
+        defaultTime = parseInt(defaultTimeValue, 10)
+      }
+      console.log(defaultTime)
+      timer.start({precision: 'secondTenths', startValues: {seconds: defaultTime}, target: {seconds: secondsGoal }});
+      document.querySelector('#time-trial-start button').click();
+      event.currentTarget.style = 'display:none'
+      document.querySelector('.stopButton').style = 'display:block'
+  });
 
-// $('#chronoExample .pauseButton').click(function () {
-//     timer.pause();
-// });
-$('#chronoExample .stopButton').click(function () {
-    timer.stop();
-    // secondUpdateForm.querySelector('button').click();
-});
-// $('#chronoExample .resetButton').click(function () {
-//     timer.reset();
-// });
-timer.addEventListener('secondTenthsUpdated', function (e) {
-  // 1. get the seconds input (by id?)
-  document.querySelector('.values')
-  // 2. set the seconds input value to current seconds ellapsed
-  $('#chronoExample .values').html(timer.getTimeValues().toString(['minutes', 'seconds', 'secondTenths']));
-  timerForm.querySelector('#time_trial_seconds').value = timer.getTotalTimeValues().seconds
-  secondUpdateForm.querySelector('#time_trial_seconds').value = timer.getTotalTimeValues().seconds
-});
-timer.addEventListener('started', function (e) {
-    $('#chronoExample .values').html(timer.getTimeValues().toString());
-});
-// timer.addEventListener('reset', function (e) {
-//     $('#chronoExample .values').html(timer.getTimeValues().toString());
-// });
-timer.addEventListener('targetAchieved', function (e) {
-    $('.progress_bar').html('COMPLETE!!');
-});
+  // $('#chronoExample .pauseButton').click(function () {
+  //     timer.pause();
+  // });
+  $('#chronoExample .stopButton').click(function () {
+      timer.stop();
+      // secondUpdateForm.querySelector('button').click();
+  });
+  // $('#chronoExample .resetButton').click(function () {
+  //     timer.reset();
+  // });
+  timer.addEventListener('secondTenthsUpdated', function (e) {
+    // 1. get the seconds input (by id?)
+    document.querySelector('.values')
+    // 2. set the seconds input value to current seconds ellapsed
+    $('#chronoExample .values').html(timer.getTimeValues().toString(['minutes', 'seconds', 'secondTenths']));
+    timerForm.querySelector('#time_trial_seconds').value = timer.getTotalTimeValues().seconds
+    secondUpdateForm.querySelector('#time_trial_seconds').value = timer.getTotalTimeValues().seconds
+  });
+  timer.addEventListener('started', function (e) {
+      $('#chronoExample .values').html(timer.getTimeValues().toString());
+  });
+  // timer.addEventListener('reset', function (e) {
+  //     $('#chronoExample .values').html(timer.getTimeValues().toString());
+  // });
+  timer.addEventListener('targetAchieved', function (e) {
+      $('.progress_bar').html('COMPLETE!!');
+  });
+
+}
+
