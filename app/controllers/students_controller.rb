32e733a97d8099c5452799ec030df493a7703842
James Devereux <devereuxjj@gmail.com>
2019-03-14 12:31:13 +0900

    homeworks now showing for both teacher and student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,7 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
     @lesson = Lesson.find(params[:lesson_id])
     @course = Course.find(@lesson.course_id)
+    @attendances = Attendance.where(lesson_id: @lesson.id)
+    @time_trials = TimeTrial.where(lesson_id: @lesson.id)
   end
 end
