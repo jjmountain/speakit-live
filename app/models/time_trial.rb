a56bc6b24cd395de9bae1ba31f3308400dff1136
Douglas Berkley <douglasmberkley@gmail.com>
2019-03-14 20:19:32 +0900

    action cable to handle timer actions on a time trial

--- app/models/time_trial.rb
+++ app/models/time_trial.rb
@@ -4,4 +4,20 @@ class TimeTrial < ApplicationRecord
   has_many :mistakes, dependent: :destroy
   belongs_to :attendance_a, class_name: 'Attendance', foreign_key: :attendance_a_id
   belongs_to :attendance_b, class_name: 'Attendance', foreign_key: :attendance_b_id
+
+  def broadcast_recording_start
+    ActionCable.server.broadcast("time_trial_#{id}", recording_started: true)
+  end
+
+  def broadcast_recording_stop
+    ActionCable.server.broadcast("time_trial_#{id}", recording_stopped: true)
+  end
+
+  def broadcast_recording_upload
+    ActionCable.server.broadcast("time_trial_#{id}", recording_finished: true)
+  end
+
+  def broadcast_recording_restart
+    ActionCable.server.broadcast("time_trial_#{id}", recording_restarted: true)
+  end
 end
